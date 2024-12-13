c36e41fa2fff0cf2e4f7a57c8735f0f4996a73c1
AaQib Ali <75785706+aaqibali1@users.noreply.github.com>
2024-12-13 10:56:10 +0500

    Add files via upload

--- day12.py
+++ day12.py
@@ -1,78 +1,32 @@
-with open('input.txt', 'r') as f:
-    lines = f.read().splitlines()
+with open('input.txt') as f:
+    lines = [line.rstrip() for line in f]
 
-grid = {}
-for y, line in enumerate(lines):
-    for x, c in enumerate(line):
-        grid[x + y * 1j] = c
+def solve(part: int):
+    tokens = 0
+    add = 10000000000000 if part == 2 else 0
+    for line in lines:
+        if line.startswith("Button"):
+            l = line.split(" ")
+            a = l[1].split(":")[0]
+            if a == 'A':
+                x1 = int(l[2][2:-1])
+                y1 = int(l[3][2:])
+            else:
+                x2 = int(l[2][2:-1])
+                y2 = int(l[3][2:])
+            # print(a,x,y)
+            
+        elif line.startswith("Prize"):
+            l = line.split(" ")
+            c = int(l[1][2:-1]) + add
+            d = int(l[2][2:]) + add
+            # print(l, x, y)
+            a = (c*y2 - d*x2) / (x1*y2 - y1*x2)
+            b = (d*x1 - c*y1) / (x1*y2 - y1*x2)
+            if a == int(a) and b == int(b):
+                tokens += int(3 * a + b)
 
-def flood_fill(grid, start):
-    region = set([start])
-    symbol = grid[start]
-    queue = [start]
-    while queue:
-        pos = queue.pop()
-        for d in [1, -1, 1j, -1j]:
-            new_pos = pos + d
-            if new_pos in grid and new_pos not in region and grid[new_pos] == symbol:
-                region.add(new_pos)
-                queue.append(new_pos)
-    return region
+    print(tokens)
 
-
-regions = []
-
-uncovered = set(grid.keys())
-while len(uncovered) > 0:
-    start = uncovered.pop()
-    region = flood_fill(grid, start)
-    uncovered -= region
-    regions.append((grid[start], region))
-
-def get_area(region):
-    return len(region[1])
-
-def get_perimeter(region):
-    perimeter = 0
-    for pos in region[1]:
-        for d in [1, -1, 1j, -1j]:
-            new_pos = pos + d
-            if new_pos not in region[1]:
-                perimeter += 1
-    return perimeter
-
-price = 0
-for region in regions:
-    area, perimeter = get_area(region), get_perimeter(region)
-    price += area * perimeter
-
-print("Part 1:", price)
-
-def get_sides_count(region):
-    perimeter_objects = set()
-    for pos in region[1]:
-        for d in [1, -1, 1j, -1j]:
-            new_pos = pos + d
-            if new_pos not in region[1]:
-                perimeter_objects.add((new_pos, d))
-    
-    distinct_sides = 0
-    while len(perimeter_objects) > 0:
-        pos, d = perimeter_objects.pop()
-        distinct_sides += 1
-        next = pos + d * 1j
-        while (next, d) in perimeter_objects:
-            perimeter_objects.remove((next, d))
-            next += d * 1j
-        next = pos + d * -1j
-        while (next, d) in perimeter_objects:
-            perimeter_objects.remove((next, d))
-            next += d * -1j
-    return distinct_sides
-
-price = 0
-for region in regions:
-    area, sides = get_area(region), get_sides_count(region)
-    price += area * sides
-    
-print("Part 2:", price)
+solve(1)
+solve(2)
