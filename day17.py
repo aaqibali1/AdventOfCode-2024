f4213df5ebeb74e1f29e7a3ceb3be498fd734a3a
AaQib Ali <75785706+aaqibali1@users.noreply.github.com>
2024-12-17 12:54:13 +0500

    day17.py

--- day17.py
+++ day17.py
@@ -1,93 +1,150 @@
-import copy
+#####part-one####
 
 with open('input.txt') as f:
-    registers, programs = f.read().split('\n\n')
+    registers,programs = f.read().split('\n\n')
 
-# Parsing the input data
-registers_original = {line.split(': ')[0].split()[1]: int(line.split(': ')[1]) for line in registers.splitlines()}
-programs = [int(num) for num in programs.replace('\n', '').split()[1].split(',')]
+registers = {line.split(': ')[0].split()[1]: int(line.split(': ')[1]) for line in registers.splitlines()}
+programs = [int(num) for num in programs.replace('\n','').split()[1].split(',')]
 
-# Functions for the operations
-def combo(num, registers):
-    if num <= 3:
+outputs = []
+
+def combo(num,registers):
+    if num<=3:
         return num
-    elif num == 4:
+    elif num==4:
         return registers['A']
     elif num == 5:
         return registers['B']
     elif num == 6:
         return registers['C']
 
-def adv(num, registers, instr, outputs):
-    registers['A'] = registers['A'] // (2 ** combo(num, registers))
-    return registers, instr + 2, outputs
+def adv(num,registers,instr):
+    registers['A']=registers['A']//(2**combo(num,registers))
+    return registers,instr+2
 
-def bxl(num, registers, instr, outputs):
-    registers['B'] = registers['B'] ^ num
-    return registers, instr + 2, outputs
+def bxl(num,registers,instr):
+    registers['B'] = registers['B']^num
+    return registers,instr+2
 
-def bst(num, registers, instr, outputs):
-    registers['B'] = combo(num, registers) % 8
-    return registers, instr + 2, outputs
+def bst(num,registers,instr):
+    registers['B'] = combo(num,registers)%8
+    return registers,instr+2
 
-def jnz(num, registers, instr, outputs):
+def jnz(num,registers,instr):
     if registers['A'] == 0:
-        return registers, instr + 2, outputs
+        return registers,instr+2
     else:
-        return registers, num, outputs
+        return registers,num
 
-def bxc(num, registers, instr, outputs):
-    registers['B'] = registers['B'] ^ registers['C']
-    return registers, instr + 2, outputs
+def bxc(num,registers,instr):
+    registers['B'] = registers['B']^registers['C']
+    return registers,instr+2
 
-def out(num, registers, instr, outputs):
-    outputs.append(combo(num, registers) % 8)
-    return registers, instr + 2, outputs
+def out(num,registers,instr):
+    outputs.append(combo(num,registers)%8)
+    return registers,instr+2
 
-def bdv(num, registers, instr, outputs):
-    registers['B'] = registers['A'] // (2 ** combo(num, registers))
-    return registers, instr + 2, outputs
+def bdv(num,registers,instr):
+    registers['B']=registers['A']//(2**combo(num,registers))
+    return registers,instr+2
 
-def cdv(num, registers, instr, outputs):
-    registers['C'] = registers['A'] // (2 ** combo(num, registers))
-    return registers, instr + 2, outputs
+def cdv(num,registers,instr):
+    registers['C']=registers['A']//(2**combo(num,registers))
+    return registers,instr+2
 
-# Part 1: Execute the program and print the output sequence
-outputs = []
-registers = copy.deepcopy(registers_original)
+length = len(programs)
 instr = 0
-while instr < len(programs):
+while instr in range(length):
     opcode = programs[instr]
-    function = [adv, bxl, bst, jnz, bxc, out, bdv, cdv][opcode]
-    num = programs[instr + 1]
-    registers, instr = function(num, registers, instr, outputs)
-
+    function = [adv,bxl,bst,jnz,bxc,out,bdv,cdv][opcode]
+    num = programs[instr+1]
+    registers,instr = function(num,registers,instr)
+    
 answer = ','.join(str(num) for num in outputs)
-print("Part 1 output:", answer)
+print(answer)
 
-# Part 2: Find the smallest valid 'a' for which the program produces the correct output sequence
+
+
+
+#####--Part-Two###
+
+
+
+import copy
+
+with open('input.txt') as f:
+    registers,programs = f.read().split('\n\n')
+
+registers_original = {line.split(': ')[0].split()[1]: int(line.split(': ')[1]) for line in registers.splitlines()}
+programs = [int(num) for num in programs.replace('\n','').split()[1].split(',')]
+
+def combo(num,registers):
+    if num<=3:
+        return num
+    elif num==4:
+        return registers['A']
+    elif num == 5:
+        return registers['B']
+    elif num == 6:
+        return registers['C']
+
+def adv(num,registers,instr,outputs):
+    registers['A']=registers['A']//(2**combo(num,registers))
+    return registers,instr+2,outputs
+
+def bxl(num,registers,instr,outputs):
+    registers['B'] = registers['B']^num
+    return registers,instr+2,outputs
+
+def bst(num,registers,instr,outputs):
+    registers['B'] = combo(num,registers)%8
+    return registers,instr+2,outputs
+
+def jnz(num,registers,instr,outputs):
+    if registers['A'] == 0:
+        return registers,instr+2,outputs
+    else:
+        return registers,num,outputs
+
+def bxc(num,registers,instr,outputs):
+    registers['B'] = registers['B']^registers['C']
+    return registers,instr+2,outputs
+
+def out(num,registers,instr,outputs):
+    outputs.append(combo(num,registers)%8)
+    return registers,instr+2,outputs
+
+def bdv(num,registers,instr,outputs):
+    registers['B']=registers['A']//(2**combo(num,registers))
+    return registers,instr+2,outputs
+
+def cdv(num,registers,instr,outputs):
+    registers['C']=registers['A']//(2**combo(num,registers))
+    return registers,instr+2,outputs
+
+
 def get_output(a):
     outputs = []
     registers = copy.deepcopy(registers_original)
     registers['A'] = a
     length = len(programs)
     instr = 0
-    while instr < length:
+    while instr in range(length):
         opcode = programs[instr]
-        function = [adv, bxl, bst, jnz, bxc, out, bdv, cdv][opcode]
-        num = programs[instr + 1]
-        registers, instr, outputs = function(num, registers, instr, outputs)
+        function = [adv,bxl,bst,jnz,bxc,out,bdv,cdv][opcode]
+        num = programs[instr+1]
+        registers,instr,outputs = function(num,registers,instr,outputs)
     return outputs
 
 valid = [0]
-for length in range(1, len(programs) + 1):
+for length in range(1,len(programs)+1):
     oldvalid = valid
     valid = []
     for num in oldvalid:
         for offset in range(8):
-            newnum = 8 * num + offset
+            newnum = 8*num+offset
             if get_output(newnum) == programs[-length:]:
                 valid.append(newnum)
-
-answer_part2 = min(valid)
-print("Part 2 answer:", answer_part2)
+                
+answer = min(valid)
+print(answer)
